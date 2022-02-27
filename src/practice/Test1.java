7289db52e1a9e6fc2d15bb9fc4c8900cb5c89fba
Psd29 <prasathambika4433@gmail.com>
2022-02-27 18:26:52 +0530

    project file

--- src/practice/Test1.java
+++ src/practice/Test1.java
@@ -1,26 +1,14 @@
 package practice;
 
-public class Test1 extends Test {
+public class Test1  {
 	
-	public void fixed() {
-		System.out.println("fixed is 9%");
 
-	}
-	@Override
-	public void Savings() {
-		System.out.println("Savings is 11%");
-		super.Savings();
-
-	}
-	
 	public static void main(String[] args) {
-		Test1 d = new Test1();
-		d.Savings();
-		d.fixed();
-		d.deposit();
+		int a = 100;
+		int b = 50;
+		int big = a>b?a:b;
+		System.out.println(big);
 	}
-	
-	
 	
 	
 	
